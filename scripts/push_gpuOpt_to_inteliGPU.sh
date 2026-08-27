262ab05f62d465fe17f8adea1f82b57306f84c5f
chrisarg <chrisarg@gmail.com>
2026-08-27 05:26:02 -0600

    deleted rogue nvidia binaries

--- scripts/push_gpuOpt_to_inteliGPU.sh
+++ scripts/push_gpuOpt_to_inteliGPU.sh
@@ -18,6 +18,7 @@ EXACT_PATHS=(
   benchmark/cpu_param_sweep.c
   tests/test_bit.c
   tests/test_offload.c
+  scripts/benchmark_config_cpu.json
 )
 RECURSIVE_DIRS=(include)
 DIRECT_FILE_DIRS=(src scripts)
